7f85ff5b181ee79483b2d5917dc1c352157fb31d
João P. Bernardino <112660754+joaoBernardinoo@users.noreply.github.com>
2024-08-06 17:20:55 -0300

    Create Avaliacao.java

---
+++ Avaliacao.java
@@ -0,0 +1,12 @@
+public class Avaliacao {
+    private Fornecedor fornecedorAvaliado;
+    private int nota;
+
+    public Fornecedor getFornecedorAvaliado() {
+        return fornecedorAvaliado;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+}
